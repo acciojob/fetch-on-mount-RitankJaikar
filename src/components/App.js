d208e5c62cbef935b3e7476414135ab816919249
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-25 12:16:32 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ const App = () => {
       <h1>Posts</h1>
       {posts.map(post => (
         <div key={post.id}>
-          <h2>{post.title}</h2>
+          <h2>1. {post.title}</h2>
           <p>{post.body}</p>
         </div>
       ))}
